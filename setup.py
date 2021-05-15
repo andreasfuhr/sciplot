597e408e894b7b0807ecc51748eb41540dcf5b4f
andreasfuhr <69312650+andreasfuhr@users.noreply.github.com>
2021-05-15 23:11:52 +0200

    Officially 0.6.1 version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='neat-sciplots',
-    version='0.5.0',
+    version='0.6.1',
     author='Andreas Führ',
     author_email='andreas.fuhr@outlook.com',
     license='MIT',
