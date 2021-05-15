1c4609c4a9ffe210dea352c6c82ecc79da72679b
andreasfuhr <andreas.fuhr@outlook.com>
2021-05-15 17:05:14 +0200

    Created setup file

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='neat-sciplots',
+    version='0.1.0',
+    packages=['src'],
+    url='https://github.com/andreasfuhr/neat-sciplots',
+    license='MIT',
+    author='Andreas Führ',
+    author_email='andreas.fuhr@outlook.com',
+    description='Neatly format Matplotlib scientific plots',
+    python_requires='>=3.4',
+    install_requires=['matplotlib', 'pyyaml', 'seaborn'],
+)
