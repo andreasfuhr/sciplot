61d773796f9facb7f467340f171cc04f43d6c07d
andreasfuhr <andreas.fuhr@outlook.com>
2021-05-16 03:38:17 +0200

    Continud to change setup.py to include parameters files successfully. New version 0.7.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='neat-sciplots',
-    version='0.7.2',
+    version='0.7.3',
     author='Andreas Führ',
     author_email='andreas.fuhr@outlook.com',
     license='MIT',
@@ -23,7 +23,8 @@ setuptools.setup(
         'Topic :: Scientific/Engineering'
     ],
     packages=setuptools.find_packages(include=['sciplot']),
-    package_data={'parameters': ['*']},
+    package_dir={'': 'sciplot'},
+    package_data={'sciplot': ['parameters/*.yml', '../README.md']},
     include_package_data=True,
     python_requires='>=3.7',
     install_requires=['matplotlib>=3.3.4', 'pyyaml', 'seaborn'],
