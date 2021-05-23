4eb8cb55615e3c9f144475117b35e00bdd788bb2
andreasfuhr <andreas.fuhr@outlook.com>
2021-05-23 21:28:31 +0200

    Implementet tests (28 in total) to test sciplot.main.style() for different themes and settings

--- tests/sciplot/test_main.py
+++ tests/sciplot/test_main.py
@@ -1,57 +1,213 @@
 import pytest
+import matplotlib.pyplot as plt
+import numpy as np
 from pathlib import Path
 import sys
 
 sys.path.append(str(Path(__file__).parent / '..' / '..'))
-from sciplot.main import *
+import sciplot.main as sciplot
 
 
 def test_get_parameters_dir():
     parameters_dir = str(Path(__file__).parent / '..' / '..' / 'sciplot' / 'parameters')
     print(parameters_dir)
-    assert get_parameters_dir() == str(parameters_dir)
+    assert sciplot.get_parameters_dir() == str(parameters_dir)
 
 
-def test_get_default_theme_lst():
-    default_theme_lst = ['basic', 'typesetting', 'colors_light', 'fonts_cm_sans_serif']
-    assert get_default_theme_lst() == default_theme_lst
+def test_get_theme_priority_lst():
+    theme_priority_lst= [
+        'alpha',
+        'beta',
+        'gamma',
+        'no-latex',
+        'serif',
+        'sans-serif',
+        'dark',
+        'default'
+    ]
+    assert sciplot.get_theme_priority_lst() == theme_priority_lst
 
 
 def test_get_theme_lst_with_string():
     theme = 'theme'
-    assert get_theme_lst(theme) == [theme]
+    assert sciplot._get_theme_lst(theme) == [theme]
+
+def test_get_theme_lst_with_capital_string():
+    theme = 'THEME'
+    assert sciplot._get_theme_lst(theme) == [theme.lower()]
 
 
 def test_get_theme_lst_with_list():
     theme = ['theme1', 'theme2']
-    assert get_theme_lst(theme) == theme
+    assert sciplot._get_theme_lst(theme) == theme
 
 
 def test_get_theme_lst_with_float():
     theme = 100.
-    with pytest.raises(SciplotException):
-        theme_lst = get_theme_lst(theme)
+    with pytest.raises(sciplot.SciplotException):
+        theme_lst = sciplot._get_theme_lst(theme)
 
 
 def test_get_theme_lst_with_float_in_list():
     theme = ['theme1', 100.]
-    with pytest.raises(SciplotException):
-        theme_lst = get_theme_lst(theme)
+    with pytest.raises(sciplot.SciplotException):
+        theme_lst = sciplot._get_theme_lst(theme)
 
 
 def test_color_lst_one_color():
     color_no = 1
     color_lst = ['#000000']
-    assert get_color_lst(color_no) == color_lst
+    assert sciplot.get_color_lst(color_no) == color_lst
 
 
 def test_color_lst_zero_colors():
     color_no = 0
-    with pytest.raises(SciplotException):
-        color_lst = get_color_lst(color_no)
+    with pytest.raises(sciplot.SciplotException):
+        color_lst = sciplot.get_color_lst(color_no)
 
 
 def test_color_lst_float_color_no():
     color_no = 2.5
-    with pytest.raises(SciplotException):
-        color_lst = get_color_lst(color_no)
+    with pytest.raises(sciplot.SciplotException):
+        color_lst = sciplot.get_color_lst(color_no)
+
+def test_style_locale_C():
+    local = 'C'
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style(locale_setting=local):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_empty():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style():
+        plt.plot(x, y)
+        plt.close()
+
+def test_style_default():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style():
+        plt.plot(x, y)
+        plt.close()
+
+def test_style_clean():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('clean'):
+        plt.plot(x, y)
+        plt.close()
+
+def test_style_dark():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('dark'):
+        plt.plot(x, y)
+        plt.close()
+
+def test_style_serif():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('serif'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_clean_sans_serif():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style(['clean', 'sans-serif']):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_no_latex():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('no-latex'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_no_latex_serif():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style(['no-latex', 'serif']):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_basic():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('basic'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_typesetting():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('typesetting'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_colors_light():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('colors_light'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_colors_dark():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('colors_dark'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_fonts_cm_sans_serif():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('fonts_cm_sans_serif'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_fonts_cm_serif():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('fonts_cm_serif'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_latex_sans_serif():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('latex_sans_serif'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_latex_serif():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style('latex_serif'):
+        plt.plot(x, y)
+        plt.close()
+
+
+def test_style_alpha_beta_gamma():
+    x = np.linspace(0, 1, 2)
+    y = 2*x
+    with sciplot.style(['alpha', 'beta', 'gamma']):
+        plt.plot(x, y)
+        plt.close()
+
