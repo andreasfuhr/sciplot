a94136ac5df9c58080bb0968acc13ab8734e121a
andreasfuhr <andreas.fuhr@outlook.com>
2021-05-23 19:20:45 +0200

    Added support for Python 3.8, 3.9

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setuptools.setup(
     url='https://github.com/andreasfuhr/sciplot',
     classifiers=[
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Development Status :: 3 - Alpha Copy',
